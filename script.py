f43afa1c771df1807b2a74cb69bc5852b8061044
IMAiCool <lcvheng@foxmail.com>
2025-05-27 01:59:57 +0800

    自动提交更新

--- script.py
+++ script.py
@@ -517,6 +517,7 @@ def main():
     initial_merge_black_white()
     conflict_processing()
     remove_top_level_domains() 
+     remove_top_and_one_level_domains()
     standardize_format()
 
 if __name__ == "__main__":
